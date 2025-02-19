cc76984322f2d1e02bf9080ba657e3f562e86087
nanimono <nan1mono@163.com>
2025-02-19 18:34:07 +0800

    init

---
+++ src/main/java/com/project/template/module/chat/serivice/FriendService.java
@@ -0,0 +1,16 @@
+package com.project.template.module.chat.serivice;
+
+import com.project.template.module.chat.pojo.entity.Friend;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author lee
+ * @since 2025-02-19
+ */
+public interface FriendService extends IService<Friend> {
+
+}
