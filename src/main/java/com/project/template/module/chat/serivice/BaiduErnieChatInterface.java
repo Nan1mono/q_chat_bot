1962ac7bddcb1d6efd4f3d95cc151e2dc33fd028
nanimono <nan1mono@163.com>
2025-02-21 18:36:10 +0800

    feat(module/chat): 百度 Ernie 聊天服务接入- 新增 BaiduErnieChatInterface 接口和 BaiduErnieChatImpl 实现类 - 添加 BaiduChatTest 测试类，用于验证百度聊天 API 接口 - 更新 .gitignore 文件，忽略 application-dev.yml 配置 - 在 pom.xml 中添加 okhttp 依赖，用于 HTTP 请求

---
+++ src/main/java/com/project/template/module/chat/serivice/BaiduErnieChatInterface.java
@@ -0,0 +1,7 @@
+package com.project.template.module.chat.serivice;
+
+public interface BaiduErnieChatInterface {
+
+    String chat(String message);
+
+}
---
+++ src/main/java/com/project/template/module/chat/serivice/impl/BaiduErnieChatImpl.java
@@ -0,0 +1,16 @@
+package com.project.template.module.chat.serivice.impl;
+
+import com.project.template.module.chat.serivice.BaiduErnieChatInterface;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaiduErnieChatImpl implements BaiduErnieChatInterface {
+
+
+
+    @Override
+    public String chat(String message) {
+        return "";
+    }
+
+}
